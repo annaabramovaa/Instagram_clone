867b4cabc1f307d3a2351bbd4fd3d5615abe4995
annaabramovaa <93001839+annaabramovaa@users.noreply.github.com>
2024-01-31 11:59:42 +0200

    removed bug

--- src/Services/PhotoService.js
+++
@@ -1,8 +0,0 @@
-const apiUrl = "http://localhost:3001";
-
-const getPhotos = async () => {
-  const response = await fetch(`${apiUrl}/pictures`);
-  return response.json();
-};
-
-export { getPhotos };
